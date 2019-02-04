5fd034163649b376c7e8f03978d97405e0bca3a2
Asim Zaidi <asimzaidih@gmail.com>
2019-02-04 12:52:16 -0800

    working on weather component :construction:

--- react-product-list/src/App.js
+++ react-product-list/src/App.js
@@ -5,16 +5,29 @@ import CategoryBtn from './CategoryBtn';
 import InventoryItem from './InventoryItem';
 import DisplayAllBtn from './DisplayAllBtn';
 
-
 class App extends Component {
 
   constructor(props) {
     super(props)
     // this.handleCategoryBtnClick = this.handleCategoryBtnClick.bind(this)
     this.state = {
-      currentCategory : null
+      currentCategory : null,
     }
     this.handleCategoryBtnClick = this.handleCategoryBtnClick.bind(this);
+  }
+
+  calculateTotalPrice() {
+    let totalPrice = inventory.filter((item) => {
+      return item.category === this.state.currentCategory || this.state.currentCategory === null
+    }).reduce((acc, item) => {
+        acc += Number(item.price);
+        // totalPrice = Math.Floor(totalPrice)
+        // acc = Math.Floor(acc)
+        return acc;
+    }, 0)
+    
+    return <h1>{totalPrice}</h1>
+    // Array.reduce((acc, item, i, arr)=>{}, init)
   }
 
   handleCategoryBtnClick(label) {    
@@ -22,7 +35,7 @@ class App extends Component {
   }
 
   getCategories() {
-    // it'll pass the item into this func and return the catargoy 
+    // it'll pass the item into this func and return the catargory 
     // cat: arr of str's
     // map takes the cat as a funtion, maps/transforms each item and returns it
     // if you put the stuff on one line the inner return is implied.
@@ -38,16 +51,29 @@ class App extends Component {
   }
 
   getInventory() {
-    return inventory.filter((item) => {
+    // let currTotal = 0;
+    const item = inventory.filter((item) => {
       return item.category === this.state.currentCategory || this.state.currentCategory === null
     }).map(( { id, name, price, description } ) =>
-      <InventoryItem
+        <InventoryItem
           key = { id }
           name = { name }
           price = { price }
-          description = { description } 
-      />
+          description = { description }
+        />
     )
+    // for (var i = 0; i < item.length; i++) {
+    //   // console.log("item:", item[i].props.price);
+    //   currTotal += parseFloat(item[i].props.price)
+    // }
+    // this.setState({ total: currTotal })
+    // console.log("total:", this.state.total);
+    
+    // console.log("currTotal:", currTotal);
+    
+
+    // total += {price}
+    return item
   }
 
   getDisplayBtn() {
@@ -63,15 +89,18 @@ class App extends Component {
         <h1 className="title">Asims Product Store</h1>
         <p>Current Category: { this.state.currentCategory }</p>
 
-        <ul className="grid-item-display">
+        <ul className="grid-display eight-gi">
           { this.getCategories() }
         </ul>
 
         <p>Current Category: { this.state.currentCategory || "All Products" }</p>
+        <h4>Sum of all prices: </h4>
+        {this.calculateTotalPrice()}
         { this.getDisplayBtn() }
         {/* <button onClick={ this.handleCategoryBtnClick } className="btn waves-effect waves-light">Display All Products</button> */}
         <div className="inventory-container">
         {this.getInventory()}
+
         </div>
 
       </div>
--- react-product-list/src/InventoryItem.js
+++ react-product-list/src/InventoryItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function InventoryItem(props) {
     return (
-        <div key={props.id} className="grid-item-display inventory-items">
+        <div key={props.id} className="grid-display three-gi inventory-items">
         <p><strong>{props.name}</strong></p>
         <p>{props.price}</p>
         <p >{ props.description }</p>
